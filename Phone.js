ef2ddac2c7efb9164b63366efb9c451009b27b4d
Ashrafull islam <mmmdashrafull211@gmail.com>
2024-03-20 23:39:19 -0700

    some empliment

--- Phone.js
+++ Phone.js
@@ -1,24 +1,32 @@
-const LoadData =async () =>{
-    const response= await fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
+const LoadData =async (phonesBrand) =>{
+    const response= await fetch(`https://openapi.programming-hero.com/api/phones?search=${phonesBrand}`);
     const data=await response.json();
     const phone=data.data;
     disPlayPhones(phone);
 }
 const disPlayPhones= phone =>{
     const CardContainer=document.getElementById("Card_container");
+    CardContainer.innerText="";
      phone.forEach(Phone => {
-        console.log(phone)
+        
          const div=document.createElement("div");
-        div.classList=`card w-96 bg-gray-100 shadow-xl`;
-         div.innerHTML=`<figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        div.classList=`card w-96 bg-gray-100 shadow-xl p-8`;
+         div.innerHTML=`<figure><img src="${Phone.image}" alt="Shoes" /></figure>
          <div class="card-body">
-           <h2 class="card-title">Shoes!</h2>
-           <p>If a dog chews shoes whose shoes does he choose?</p>
-           <div class="card-actions justify-end">
-             <button class="btn btn-primary">Buy Now</button>
+           <h2 class="card-title">|${Phone.phone_name}</h2>
+           <p>There are many variations of passages of available, but the majority have suffered</p>
+           <div class="card-actions justify-center">
+             <button class="btn btn-primary">Show Details</button>
            </div>
          </div>`;
          CardContainer.appendChild(div);
      });
 }
-LoadData();
+
+const SearchText=()=>{
+    const CatchId=document.getElementById("Intput_text");
+    const CatchIdValue=CatchId.value;
+    CatchId.value="";
+    LoadData(CatchIdValue);
+    
+}
